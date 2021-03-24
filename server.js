f5453be98d0b05885bedc38f5a1d3cda51210bf5
Flautisimo <flautisimo@gmail.com>
2021-03-24 09:58:55 +0200

    This is my first commit message

---
+++ server.js
@@ -0,0 +1,57 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
+
+// If there is no date input, we create a separate endpoint //
+app.get("/api/timestamp/", (req, res) => {
+  res.json({ unix: Date.now(), utc: Date() });
+});
+
+// If we get an input //
+app.get('/api/timestamp/:date?', (req, res) => {
+  let inputString = req.params.date;
+// We validate the string given in case it begins with more than four digits //
+  if (/\d{5,}/.test(inputString)) {
+    const dateNumber = parseInt(inputString);
+    
+    res.json({ unix: dateNumber, utc: new Date(dateNumber).toUTCString() });
+  } else {
+    let dateObject = new Date(inputString);
+    
+    if (dateObject.toString() === "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({ unix: dateObject.valueOf(), utc: new Date(dateObject).toUTCString() });
+    }
+  }   
+});
